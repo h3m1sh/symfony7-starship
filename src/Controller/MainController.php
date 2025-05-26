661c64359ae760dd1cf47e90171898bd0dd4b6d3
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-05-26 13:50:37 +1200

    cs-fixer & twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Hello World!');
+    }
+}
